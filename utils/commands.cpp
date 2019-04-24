01bf0b0a5f90062d2ab84971976c20385e2c9ce4
Ori0nZ <manhtranduc1111@gmail.com>
2019-04-24 21:51:21 +0300

    In home edit

--- utils/commands.cpp
+++ utils/commands.cpp
@@ -3,33 +3,61 @@
 #include <errno.h>
 #include <wiringSerial.h>
 #include <stdlib.h>
-int main ()
+#include <time.h>
+#include <unistd.h>
+void drive_funtion(unsigned char fun[4])
+{
+  for (int i = 0; i < 4; i++)
+  {
+    serialPutchar(port_num, fun[i]);
+    printf("%c", fun[i]);
+  }
+}
+int main()
 {
   int port_num;
   printf("Connection...");
-  if ((port_num = serialOpen ("/dev/ttyS0", 9600)) < 0)
+  if ((port_num = serialOpen("/dev/ttyS0", 9600)) < 0)
   {
-    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-    return 1 ;
+    fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+    return 1;
   }
-  printf("Successful\n");
-	unsigned char arraycomd[4] = {"FWD", "STP", "LFT", "RFT"};
-    printf("Do command: ");
-    
-      unsigned char tx;
-      tx = arraycomd[0];
-	serialPutchar(port_num, tx);
-	sleep(2000);
-	tx = arraycomd[3];
-	  serialPutchar(port_num, tx);
-	  sleep(2000);
-	  tx = arraycomd[2];
-	  serialPutchar(port_num, tx);
-	  sleep(2000);
-	  tx = arraycomd[1];
-	  serialPutchar(port_num, tx);
-	  sleep(2000);
-    }
+  printf("Connected Successful\n");
+  unsigned char left[] = "LFT!";
+  unsigned char right[] = "RFT!";
+  unsigned char stop[] = "STP!";
+  unsigned char go_on[] = "SFT!";
+  while (1)
+  {
+    drive_funtion(left);
+    usleep(1000);
+    drive_funtion(right);
+    usleep(1000);
+    drive_funtion(go_on);
+    usleep(1000);
+    drive_funtion(stop);
+    usleep(1000);
   }
-}
 
+  /*while (1)
+  {
+    printf("Sent: ");
+    while (1)
+    {
+      unsigned char tx;
+      scanf("%c", &tx);
+      if (tx == '\n')
+      {
+        tx = '!';
+        serialPutchar(port_num, tx);
+        fflush(stdout);
+        break;
+      }
+      if (tx != '\0')
+      {
+        serialPutchar(port_num, tx);
+      }
+      fflush(stdout);
+    }
+  }*/
+}
